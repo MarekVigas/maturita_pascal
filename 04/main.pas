714a47441bb5a05e961626271fe25970d17b87bf
Unknown <vigas147@gmail.com>
2017-12-07 12:35:25 +0100

    Zmena z real na integer v ulohe 4

--- 04/main.pas
+++ 04/main.pas
@@ -1,16 +1,18 @@
 program uloha4;
-  var c,h,m: real;
+  var c,h,m: integer;
   
 begin
     write('Zadaj pocet sekund:');
     readln(c);
+    h := 0;
+    m := 0;  
     while c >= 3600 do begin
       c := c - 3600;
       h := h + 1;
     end;
     while c >= 60 do begin
       c := c - 60;
-      m:= m + 1;
+      m := m + 1;
     end;
-    writeln(h:0:0, ':', m:0:0, ':', c:0:0);
+    writeln(h, 'h:', m, 'm:', c, 's');
 end.
