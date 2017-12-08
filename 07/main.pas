dd0271cf80c9b194bd05f723a98e75b6cba54a4d
Unknown <vigas147@gmail.com>
2017-12-08 08:44:23 +0100

    uloha 7

---
+++ 07/main.pas
@@ -0,0 +1,12 @@
+program uloha7;
+	var a,i,sum:Integer;
+begin
+	write('Zadaj číslo pre vypočet faktoriálu:');
+	read(a);
+	sum := 1;
+	for i := a downto 1 do
+	begin
+		sum := sum * i;
+	end;
+	writeln('Faktoriál čísla ',a, ' je ', sum);
+end.
