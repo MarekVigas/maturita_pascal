acadd24950eaf0ce31c88b952f0c178cf376599c
Unknown <vigas147@gmail.com>
2018-01-09 13:25:47 +0100

    uloha 9

---
+++ 09/main.pas
@@ -0,0 +1,28 @@
+program uloha9;
+	var
+		pole_cisel: array[0..20] of	Integer;
+	var
+		i,min,min_pos: Integer;
+begin
+	randomize;
+
+	min := 999;
+
+	for i := 0 to Length(pole_cisel) - 1 do
+	begin
+		pole_cisel[i] := random(20);
+		write(pole_cisel[i]:4);
+	end;
+
+	writeln();
+	
+	for i := 0 to Length(pole_cisel) - 1 do
+	begin
+		if pole_cisel[i] < min then
+		begin
+			min := pole_cisel[i];
+		end;
+	end;
+
+	writeln('Minimum je ', min);
+end.
